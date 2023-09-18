b8eaa88a563a248fd12f4f09584479cb5057fbad
broyettore <broyettore@gmail.com>
2023-09-18 16:42:10 +0200

    Created static methods to handle user interaction keeping program clean

--- csharp-gestore-eventi/Classes/EventProgram.cs
+++ csharp-gestore-eventi/Classes/EventProgram.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Reflection.Metadata;
 using System.Text;
@@ -72,8 +73,137 @@ namespace csharp_gestore_eventi.Classes
                 Console.WriteLine(e);
             }
         }
-        
 
+        public static string GetProgamName()
+        {
+            Console.Write("How would you like to name your program of events: ");
+            return Console.ReadLine();
+        }
+        
+        public static int GetProgamNumOfEvents() 
+        {
+            Console.Write("How many events would you like to insert in your program: ");
+            int eventsNumber;
+            while (int.TryParse(Console.ReadLine(), out eventsNumber) == false)
+            {
+                Console.WriteLine("Please insert a valid number");
+            }
+            return eventsNumber;
+        }
+
+        public static Event createEvent(int eventsNumber)
+        {
+            // asks user event name
+            Console.Write($"How would you like to name your event {eventsNumber}: ");
+            string eventName = Console.ReadLine();
+
+            // asks user event date
+            Console.Write($"When should the event {eventsNumber} take place (example: 10 january 2020): ");
+            DateTime parsedDate;
+
+            // loop unitl date is written correctly
+            while (DateTime.TryParseExact(Console.ReadLine(), "dd MMMM yyyy HH:mm", CultureInfo.InvariantCulture, DateTimeStyles.None, out parsedDate) == false)
+            {
+                Console.Write("Date is written wrong: ");
+            }
+
+            // asks user max capacity of the event
+            Console.Write($"What should be the max capacity of your event {eventsNumber}: ");
+            int totalSeats;
+
+            while (int.TryParse(Console.ReadLine(), out totalSeats) == false)
+            {
+                Console.WriteLine("Please insert a number: ");
+            }
+
+            Console.WriteLine(); // empty line
+
+
+            // Create and store the events in the array
+            return new Event(eventName, parsedDate, totalSeats);
+        }
+
+        public static void addSeatBooking(Event e)
+        {
+            //asks user if and how many seats should be booked until he is satisfied               
+            Console.Write("Would you like to book some seats (yes / no): ");
+            string bookingAnswer = Console.ReadLine();
+
+            if (bookingAnswer == "yes")
+            {
+                Console.Write("How many seats should be booked: ");
+                int seatToBook = int.Parse(Console.ReadLine());
+
+                int seatsBooked = e.bookSeats(seatToBook);
+                int seatsAvailable = e.GetEventMaxCapacity() - seatsBooked;
+
+                Console.WriteLine(); // empty line
+
+                Console.WriteLine($"This is the number of seats booked in event {e.eventTitle}: {seatsBooked}");
+                Console.WriteLine($"This is the number of seats available in event {e.eventTitle}: {seatsAvailable} \n");
+            }
+            else
+            {
+                Console.WriteLine("Ok then thank you ! \n");
+            }
+        }
+
+        public static void cancelSeatBooking(Event e)
+        {
+            // asks user if and how many seats booking he wants to cancel until he is satisfied
+
+            Console.Write("Would you like to cancel some seats bookings (yes / no): ");
+            string cancelBookingAnswer = Console.ReadLine();
+
+            if (cancelBookingAnswer == "yes")
+            {
+                Console.Write("How many seats booking should be cancelled: ");
+                int seatToCancel = int.Parse(Console.ReadLine());
+
+                int seatsBooked = e.cancelSeatBooking(seatToCancel);
+                int seatsAvailable = e.GetEventMaxCapacity() - seatsBooked;
+
+                Console.WriteLine(); // empty line
+
+                Console.WriteLine($"This is the number of seats booked (updated) in event {e.eventTitle}: {seatsBooked}");
+                Console.WriteLine($"This is the number of seats available in event (updated) {e.eventTitle}: {seatsAvailable} \n");
+            }
+            else
+            {
+                Console.WriteLine("Ok then thank you ! \n");
+            }
+        }
+
+        public static void PrintEventSummary(EventProgram program)
+        {
+            int numberOfEvents = program.GetTotalEvents();
+
+            // Prints your event
+            Console.WriteLine($"This is the total number of programmed events: {numberOfEvents}");
+            Console.WriteLine(); // empty line
+
+            Console.WriteLine("These are the events listed: \t");
+            EventProgram.PrintEventList(program.Events);
+
+            Console.WriteLine(); // empty line
+
+            Console.Write("Insert a date to see the events of that date (example: 18 september 2023): ");
+            DateTime inputDate;
+            while (DateTime.TryParseExact(Console.ReadLine(), "dd MMMM yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out inputDate) == false)
+            {
+                Console.Write("Date is written wrong: ");
+            }
+
+            Console.WriteLine($"these are the events taking place on {inputDate} : \t");
+            foreach (var e in program.GetEventFromDate(inputDate))
+            {
+                Console.WriteLine(e);
+            }
+            Console.WriteLine(); // empty line
+
+            Console.WriteLine("This is the program for this month: \n\t");
+            program.PrintEventDateTitle();
+        }
         
     }
 }
--- csharp-gestore-eventi/Program.cs
+++ csharp-gestore-eventi/Program.cs
@@ -10,19 +10,12 @@ namespace csharp_gestore_eventi
         {
             Console.WriteLine("Welcome to Event Handler 2.0 \n ");
 
-            Console.Write("How would you like to name your program of events: ");
-            string programName = Console.ReadLine();
+            string programName = EventProgram.GetProgamName();
 
             Console.WriteLine(); // empty line
 
-            Console.Write("How many events would you like to insert in your program: ");
-            int eventsNumber;
-
-            while (int.TryParse(Console.ReadLine(), out eventsNumber) == false)
-            {
-                Console.WriteLine("Please insert a number");
-            }
-
+            int eventsNumber = EventProgram.GetProgamNumOfEvents();
+             
             Console.WriteLine(); // empty line         
 
             // Program instance created
@@ -31,91 +24,17 @@ namespace csharp_gestore_eventi
             // your created events
             Event[] myEvents = new Event[eventsNumber];
 
-
-
-
             for (int i = 0; i < eventsNumber; i++)
             {
                 try
                 {
-                    // asks user event name
-                    Console.Write($"How would you like to name your event {i + 1}: ");
-                    string eventName = Console.ReadLine();
-
-                    // asks user event date
-                    Console.Write($"When should the event {i + 1} take place (example: 10 january 2020): ");
-                    DateTime parsedDate;
-
-                    // loop unitl date is written correctly
-                    while (DateTime.TryParseExact(Console.ReadLine(), "dd MMMM yyyy HH:mm", CultureInfo.InvariantCulture, DateTimeStyles.None, out parsedDate) == false)
-                    {
-                        Console.Write("Date is written wrong: ");
-                    }
-
-                    // asks user max capacity of the event
-                    Console.Write($"What should be the max capacity of your event {i + 1}: ");
-                    int totalSeats;
-
-                    while (int.TryParse(Console.ReadLine(), out totalSeats) == false)
-                    {
-                        Console.WriteLine("Please insert a number: ");
-                    }
-
-                    Console.WriteLine(); // empty line
-
-
-                    // Create and store the events in the array
-                    myEvents[i] = new Event(eventName, parsedDate, totalSeats);
-
-
-
+                    // Creates new Event
+                    Event newEvent = EventProgram.createEvent(i + 1);
+                    myEvents[i] = newEvent;
                     // Adding events to the program
-                    newProgram.AddEvent(myEvents[i]);
-
-                    //asks user if and how many seats should be booked until he is satisfied               
-                    Console.Write("Would you like to book some seats (yes / no): ");
-                    string bookingAnswer = Console.ReadLine();
-
-                    if (bookingAnswer == "yes")
-                    {
-                        Console.Write("How many seats should be booked: ");
-                        int seatToBook = int.Parse(Console.ReadLine());
-
-                        int seatsBooked = myEvents[i].bookSeats(seatToBook);
-                        int seatsAvailable = myEvents[i].GetEventMaxCapacity() - seatsBooked;
-
-                        Console.WriteLine(); // empty line
-
-                        Console.WriteLine($"This is the number of seats booked in event {myEvents[i].eventTitle}: {seatsBooked}");
-                        Console.WriteLine($"This is the number of seats available in event {myEvents[i].eventTitle}: {seatsAvailable} \n");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Ok then thank you ! \n");
-                    }
-
-                    // asks user if and how many seats booking he wants to cancel until he is satisfied
-
-                    Console.Write("Would you like to cancel some seats bookings (yes / no): ");
-                    string cancelBookingAnswer = Console.ReadLine();
-
-                    if (cancelBookingAnswer == "yes")
-                    {
-                        Console.Write("How many seats booking should be cancelled: ");
-                        int seatToCancel = int.Parse(Console.ReadLine());
-
-                        int seatsBooked = myEvents[i].cancelSeatBooking(seatToCancel);
-                        int seatsAvailable = myEvents[i].GetEventMaxCapacity() - seatsBooked;
-
-                        Console.WriteLine(); // empty line
-
-                        Console.WriteLine($"This is the number of seats booked (updated) in event {myEvents[i].eventTitle}: {seatsBooked}");
-                        Console.WriteLine($"This is the number of seats available in event (updated) {myEvents[i].eventTitle}: {seatsAvailable} \n");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Ok then thank you ! \n");
-                    }
+                    newProgram.AddEvent(newEvent);
+                    EventProgram.addSeatBooking(newEvent);
+                    EventProgram.cancelSeatBooking(newEvent);
 
                 }
                 catch (Exception ex) // if an exception is caugth it loops into asking event[i] info until insertions are correct
@@ -127,33 +46,8 @@ namespace csharp_gestore_eventi
                 }
             }
 
-            int numberOfEvents = newProgram.GetTotalEvents();
-
-            // Prints your event
-            Console.WriteLine($"This is the total number of programmed events: {numberOfEvents}");
-            Console.WriteLine(); // empty line
-
-            Console.WriteLine("These are the events listed: \t");
-            EventProgram.PrintEventList(newProgram.Events);
-
-            Console.WriteLine(); // empty line
-
-            Console.Write("Insert a date to see the events of that date (example: 18 september 2023): ");
-            DateTime inputDate;
-            while (DateTime.TryParseExact(Console.ReadLine(), "dd MMMM yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out inputDate) == false)
-            {
-                Console.Write("Date is written wrong: ");
-            }
-
-            Console.WriteLine($"these are the events taking place on {inputDate} : \t");
-            foreach (var e in newProgram.GetEventFromDate(inputDate))
-            {
-                Console.WriteLine(e);
-            }
-            Console.WriteLine(); // empty line
-
-            Console.WriteLine("This is the program for this month: \t");
-            newProgram.PrintEventDateTitle();
+            // Print all info related to the events in the program
+            EventProgram.PrintEventSummary(newProgram);
         }
     }
 }
